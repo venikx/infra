28761f99fce62fb9707c6cb01a8c586600910c98
Kevin De Baerdemaeker <code@venikx.com>
2026-04-25 17:04:05 +0300

    feat: add rsync truenas public ssh key

--- homelab/hosts/vps-hz-prod-svc1/default.nix
+++ homelab/hosts/vps-hz-prod-svc1/default.nix
@@ -93,6 +93,17 @@ in
     };
   };
 
+  users.users.cdn-sync = {
+    isSystemUser = true;
+    group = config.services.nginx.group;
+    home = "/srv/cdn";
+    createHome = true;
+    shell = pkgs.bash;
+    openssh.authorizedKeys.keys = [
+      ''command="${pkgs.rrsync}/bin/rrsync /srv/cdn",restrict ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDcKD2ZNwTdJr5Tn1iFBQJTMw6m+ABHXXDVy7blAKma2qoZi4gMIsAFY3Z5/BU9Kt/+IDK2flnjFYMP5DeiKburCNOwZSTKZnXRGu+U+KH7px2aS/fLPPP8yvW25gkIab1Fn2LASEySFLdVq3sW0vvWslY+9RTI/3Gnk9h7zxzGbgS5OWaJ7ehcVpb+1dd4r/a5ZkquvQW/hZmsM6l/1d7VTK49oq8BWi/emkn83rhUA/Dy8Xw2+oZm7tqA2D98fJoi+bBZKHavpk588CKNWG9aOnr6VFQPBez6Kvig8qQhWgbLulkydvBuEEFEGOIK6BgrlXwa42p4qmErAnZZ0R938nephlFrZ7GmElGR2WOhfwOh/ySKroKrLHcDl3i0zdqO/fXetdMA8DbxYPNAJVt8MdjByENHA0PzGAq0Uk2re5sk2s3uiKu6H/EBdCclRPbz+FrwuaO0Kd1W4Af2MMD0FvtDR2rR7EgA0LdTQCXnvx76lOZUZ6u0fIe+7BkmIIs= root@truenas''
+    ];
+  };
+
   #age.secrets.secret1.rekeyFile = ../../../secret1.age;
   #services.paperless.enable = true;
   #services.paperless.passwordFile = config.age.secrets.secret1.path;
